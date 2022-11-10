223527a17fb7d06ba6f5796eb0ac0b07d40d2e01
Hamid Semix <semix.hamidouh@gmail.com>
2022-11-10 16:54:09 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+#![windows_subsystem="windows"]
+#[macro_use] extern crate sciter;
+use sciter::{ Value, HELEMENT, Element };
+use std::thread;
+struct EventHandler;
+impl EventHandler {
+    fn sum(&self, a: i32, b: i32) -> i32 { a + b }
+    fn sum_async(&self, x: i32, y: i32, callback: Value) -> () {
+        thread::spawn(move || {
+            callback.call(None, &make_args!(x + y), None).unwrap();
+        });
+    }
+    fn capitalize(&self, a: String) -> String {
+        a.to_string().as_str().to_uppercase()
+    }
+}
+
+impl sciter::EventHandler for EventHandler {
+    fn get_subscription(&mut self) -> Option<sciter::dom::event::EVENT_GROUPS> {
+		Some(sciter::dom::event::default_events() | sciter::dom::event::EVENT_GROUPS::HANDLE_METHOD_CALL)
+	}
+    fn document_complete(&mut self, root: HELEMENT, _target: HELEMENT) {
+        &Element::from(root).call_function("set_title", &make_args!("quick maths!"));
+    }
+    dispatch_script_call! (
+        fn sum(i32, i32);
+        fn sum_async(i32, i32, Value);
+        fn capitalize(Value);
+    );
+}
+
+fn main() {
+	let login = "./ui/main.html";
+
+    sciter::set_options(sciter::RuntimeOptions::DebugMode(true)).unwrap();
+    sciter::set_options(sciter::RuntimeOptions::ScriptFeatures(
+        sciter::SCRIPT_RUNTIME_FEATURES::ALLOW_SYSINFO  as u8 |
+        sciter::SCRIPT_RUNTIME_FEATURES::ALLOW_FILE_IO  as u8 |
+        sciter::SCRIPT_RUNTIME_FEATURES::ALLOW_EVAL     as u8 |
+        sciter::SCRIPT_RUNTIME_FEATURES::ALLOW_SYSINFO  as u8 
+    )).unwrap();
+    
+    let mut frame = sciter::Window::new();
+    
+    frame.load_file(login);
+
+    frame.run_app();
+
+}
